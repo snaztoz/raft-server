161aa3b673672695bb22078b4043a34b72ed3b92
snaztoz <hafidhmn@gmail.com>
2021-05-17 23:03:14 +0700

    Memberikan comment.

--- models/index.js
+++ models/index.js
@@ -1,4 +1,20 @@
 module.exports = {
+
+  // Letakkan semua model yang digunakan di sini. Berikan
+  // format seperti berikut:
+  //
+  //    NamaModel: strukturModel
+  //
+  // contoh:
+  //
+  //    const { DataTypes } = require('sequelize')
+  //
+  //    Foo: {
+  //      type: DataTypes.INTEGER
+  //      ...options...
+  //    }
+  //
+
   User: require('./User'),
   Message: require('./Message'),
   Chatroom: require('./Chatroom'),
