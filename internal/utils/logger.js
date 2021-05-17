3c4a3b9a61c9d6e732667feee991d14c380fba0b
snaztoz <hafidhmn@gmail.com>
2021-05-17 14:49:42 +0700

    Membuat fungsi untuk logging.

---
+++ internal/utils/logger.js
@@ -0,0 +1,13 @@
+templates = {
+  'INFO': '[\x1b[36m%s\x1b[0m] %s',
+  'ERR': '[\x1b[31m%s\x1b[0m] %s',
+  'SUCCESS': '[\x1b[32m%s\x1b[0m] %s',
+}
+
+module.exports = function(type, message) {
+  if (!templates.hasOwnProperty(type)) {
+    throw 'INVALID LOGGING TYPE!'
+  }
+
+  console.log(templates[type], type, message)
+}
