91850444fdfae201607544517e9d56ca7ebcf769
snaztoz <hafidhmn@gmail.com>
2021-05-17 14:51:43 +0700

    Refactor logger.

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 
 const bootstrap = require('./bootstrap')
+const log = require('./internal/utils/logger')
 
 bootstrap()
   .then(() => {
     require('./internal').app.runAt(process.env.SERVER_PORT)
   })
   .catch(() => {
-    console.log('[ERR] Failed to start the server. Exit...')
+    log('ERR', 'Failed to start the server. Exit...')
   })
