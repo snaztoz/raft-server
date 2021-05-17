a5e95124ce7b0c16b39133572d12887cc62760f1
snaztoz <hafidhmn@gmail.com>
2021-05-17 22:56:00 +0700

    Refactor step bootstrapping.

--- bootstrap/index.js
+++ bootstrap/index.js
@@ -5,53 +5,75 @@ module.exports = async function() {
 
   log('INFO', 'Bootstrapping application')
 
-  // bootstrapping server
-  log('INFO', 'Setting up the server')
-  const expressApp = require('express')()
-  const app = require('http').createServer(expressApp)
-  internal.app.setApp(app)
-  log('SUCCESS', 'Server is ready')
+  /*
+   * Bootstrapping server
+   */
+  await (async function() {
+    log('INFO', 'Setting up the server')
 
+    const expressApp = require('express')()
+    const app = require('http').createServer(expressApp)
+    internal.app.setApp(app)
 
-  // bootstrapping orm
-  log('INFO', 'Connecting to database')
-  const { Sequelize } = require('sequelize')
-  internal.orm
-    .set(
-      new Sequelize(
-        process.env.DB_NAME,
-        process.env.DB_USERNAME,
-        process.env.DB_PASSWORD,
-        {
-          host: process.env.DB_HOST,
-          dialect: process.env.DB_DIALECT
-        }
-      )
-    )
-  
-  try {
-    await internal.orm.get().authenticate()
-    log('SUCCESS', 'Database is connected')
-  } catch (err) {
-    log('ERR', `Failed at connecting to database. code: ${err}`)
-    throw err
-  }
+    log('SUCCESS', 'Server is ready')
+  })()
 
+  /*
+   * Bootstrapping ORM
+   */
+  await (async function() {
+    log('INFO', 'Connecting to database')
 
-  // bootstrapping models
-  log('INFO', 'Synchronizing models')
-  const models = require('../models')
-  for (const modelName in models) {
+    const { Sequelize } = require('sequelize')
     internal.orm
-      .get()
-      .define(modelName, models[modelName])
-  }
-  // memasang relationship antar tabel
-  require('../models/associations')(internal.orm.get())
+      .set(
+        new Sequelize(
+          process.env.DB_NAME,
+          process.env.DB_USERNAME,
+          process.env.DB_PASSWORD,
+          {
+            host: process.env.DB_HOST,
+            dialect: process.env.DB_DIALECT
+          }
+        )
+      )
 
-  await internal.orm.get().sync()
-  log('SUCCESS', 'Models are ready')
+    try {
+      await internal.orm.get().authenticate()
+    } catch (err) {
+      log('ERR', `Failed at connecting to database. code: ${err}`)
+      throw err
+    }
 
+    log('SUCCESS', 'Database is connected')
+  })()
+
+  /*
+   * Bootstrapping models
+   */
+  await (async function() {
+    log('INFO', 'Synchronizing models')
+
+    // register semua model
+    const models = require('../models')
+    for (const modelName in models) {
+      try {
+        internal.orm
+          .get()
+          .define(modelName, models[modelName])
+      } catch (err) {
+        log('ERR', `Failed at registering model ${modelName}. code: ${err}`)
+        throw err
+      }
+    }
+
+    // memasang relationship antar tabel
+    require('../models/associations')(internal.orm.get())
+
+    await internal.orm.get().sync()
+
+    log('SUCCESS', 'Models are ready')
+  })()
 
   log('SUCCESS', 'Application is ready to run!')
 }
