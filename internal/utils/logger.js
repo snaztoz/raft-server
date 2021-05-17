edbab5a59af1bc6c84b6c5a775bf9504ac2bf791
snaztoz <hafidhmn@gmail.com>
2021-05-17 15:05:01 +0700

    Mengubah format tulisan log.

--- internal/utils/logger.js
+++ internal/utils/logger.js
@@ -1,7 +1,7 @@
 templates = {
-  'INFO': '[\x1b[36m%s\x1b[0m] %s',
-  'ERR': '[\x1b[31m%s\x1b[0m] %s',
-  'SUCCESS': '[\x1b[32m%s\x1b[0m] %s',
+  'INFO': '[\x1b[36m%s\x1b[0m]\t   %s',
+  'ERR': '[\x1b[31m%s\x1b[0m]\t    %s',
+  'SUCCESS': '[\x1b[32m%s\x1b[0m]  %s',
 }
 
 module.exports = function(type, message) {
